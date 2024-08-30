59a9e1f3bdfc16a49817a9d91d80a04e28c02835
BlackPerimeter <blackperimeter@MacBook-Pro-Ivan.local>
2024-08-30 15:49:16 +0300

    Initial commit. ML has been realised, but not fully tested

--- mlseExperiment.m
+++ mlseExperiment.m
@@ -1,23 +1,28 @@
 clear all;
 close all;
 clc;
+F = findall(0,'type','figure','tag','TMWWaitbar');
+delete(F);
+fig = figure;
 
 %% Experiments parameters
 expParams = struct;
 expParams.channelLengthList = 1:5;
-expParams.blockLength = 128;
-expParams.expCount = 100;
-expParams.snrDbRange = -10:1:11;
+expParams.blockLength = 16;
+expParams.expCount = 10;
+expParams.snrDbRange = -10:2:10;
 
 %% Run experiments
-fig = figure;
 legendList = [];
+ww = waitbar(0, "start");
 % Iterate over channel lenghts
+denom = length(expParams.channelLengthList)*expParams.expCount*length(expParams.snrDbRange);
 for channelLengthIdx = 1:length(expParams.channelLengthList)
     channelLength = expParams.channelLengthList(channelLengthIdx);
     
     % Initialize equalizer
     equalizer = mlse(channelLength, expParams.blockLength);
+    idx_chan = (channelLengthIdx - 1)*expParams.expCount*length(expParams.snrDbRange);
     
     % Initialize matrix for BER
     berSnrExp = zeros(length(expParams.snrDbRange), expParams.expCount);
@@ -25,12 +30,13 @@ for channelLengthIdx = 1:length(expParams.channelLengthList)
     % Iterate over seed
     for expIdx = 1:expParams.expCount
         rng(expIdx);
-        
+        idx_exp = (expIdx - 1)*length(expParams.snrDbRange);
         % Rayleigh channel - CIR from CN(0,1)
         cir = 1/sqrt(2) * (randn(channelLength,1) + 1j * randn(channelLength,1));
         
         % Tx bits
         txBits = randi([0 1], expParams.blockLength, 1);
+        disp(txBits')
         
         % Modulate
         txSignal = 2*txBits-1;
@@ -40,10 +46,12 @@ for channelLengthIdx = 1:length(expParams.channelLengthList)
         
         % Iterate over SNR
         for snrIdx = 1:length(expParams.snrDbRange)
+            waitbar((idx_chan + idx_exp + snrIdx)/denom, ww, "chan " + num2str(channelLengthIdx) + " exp " + num2str(expIdx) + " snr " + num2str(expParams.snrDbRange(snrIdx)));
             snrDb = expParams.snrDbRange(snrIdx);
             
             % Received signal model
-            rxSignal = conv(txSignal, cir) + unitPowerNoise * db2mag(-snrDb);
+            noise = unitPowerNoise * db2mag(-snrDb);
+            rxSignal = conv(txSignal, cir) + noise;
             
             % Run equalizer
             equalized = equalizer.run(rxSignal, cir);
@@ -52,7 +60,7 @@ for channelLengthIdx = 1:length(expParams.channelLengthList)
             rxBits = double(equalized > 0);
             
             % Calculate BER
-            berSnrExp(snrIdx, expIdx) = mean(rxBits ~= txBits);
+            berSnrExp(snrIdx, expIdx) = mean(rxBits.' ~= txBits);
         end
     end
     
@@ -60,23 +68,21 @@ for channelLengthIdx = 1:length(expParams.channelLengthList)
     semilogy(expParams.snrDbRange, mean(berSnrExp, 2)); hold on;
     
     % Accumulate legend
-    legendList = [legendList; "MLSE, CIR length = "+string(channelLength)];
+    legendList = [legendList; "MLSE, CIR length = " + string(channelLength)];
 end
 
 %% Visualize results
 % Calculate BPSK AWGN BER for comparison
 awgnBer = berawgn(expParams.snrDbRange, 'psk', 2, 'nondiff');
-
 % Put on plot
 semilogy(expParams.snrDbRange, awgnBer);
 
 % Accumulate legend
 legendList = [legendList; "AWGN BER"];
-
 % Put legend, apply limits, specify axis
 legend(legendList); grid on; ylim([1e-5 1])
 xlabel("SNR, dB"); ylabel("BER");
 
 % Save figure
 saveas(fig, "results.png");
-
+close(ww);
